c193264265ff00edb7bb99c82b19a6e371b08767
saengx <157899625+saengx@users.noreply.github.com>
2025-10-27 05:49:12 +0700

    Update configure.sh

--- mobile-mining/ccminer/configure.sh
+++ mobile-mining/ccminer/configure.sh
@@ -1,5 +1,5 @@
 # To change the cuda arch, edit Makefile.am and run ./build.sh
 
-extracflags="-mfpu=neon -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16 -fuse-ld=lld-14"
+extracflags="-mfpu=neon -D_REENTRANT -falign-functions=20 -falign-jumps=20 -falign-labels=20 -fuse-ld=lld-20"
 
-./configure CC=clang-14 CXX=clang++-14 CFLAGS="-O3 -march=armv7-a $extracflags" CXXFLAGS="-O3 $extracflags -march=armv7-a"
+./configure CC=clang-20 CXX=clang++-20 CFLAGS="-O3 -march=armv7-a $extracflags" CXXFLAGS="-O3 $extracflags -march=armv7-a"
